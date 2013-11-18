814e88cd154a5965c4be80b1719799029eb9cf8a
Brenn Oosterbaan <boosterbaan@schubergphilis.com>
2013-11-18 16:06:39 +0100

    handle NMS unresponsiveness
    API timeout is 18 seconds, 20 second sleep and 1 retry for a total of 56
    seconds (just within the 60 seconds Nagios timeout).
    Tested with "setup appliance NMS restart" which it now successfully
    survives.

--- check_nexenta.py
+++ check_nexenta.py
@@ -24,15 +24,16 @@
 #                               space thresholds, added volume
 #                               compression to performance data
 #                               and extra support for HA clusters
-# 2012/11/02 v1.0.3 Brenn Oosterbaan - python 2.4 compatible
-# 2013/13/02 v1.0.4 Brenn Oosterbaan - added 2 retries for API connect
-# 2013/14/02 v1.0.5 Patrick - added https and snmp v2 support
-# 2013/15/02 v1.0.6 Brenn Oosterbaan - simplified snmp v2/v3 support
-# 2013/16/02 v1.0.7 Brenn Oosterbaan - only lookup hostname once
-# 2013/24/06 v1.0.8 Brenn Oosterbaan - added option to IGNORE messages
-# 2013/22/08 v1.0.9 Brenn Oosterbaan - added option to change only
+# 2012/02/11 v1.0.3 Brenn Oosterbaan - python 2.4 compatible
+# 2013/02/13 v1.0.4 Brenn Oosterbaan - added 2 retries for API connect
+# 2013/02/14 v1.0.5 Patrick - added https and snmp v2 support
+# 2013/02/15 v1.0.6 Brenn Oosterbaan - simplified snmp v2/v3 support
+# 2013/02/16 v1.0.7 Brenn Oosterbaan - only lookup hostname once
+# 2013/06/24 v1.0.8 Brenn Oosterbaan - added option to IGNORE messages
+# 2013/08/22 v1.0.9 Brenn Oosterbaan - added option to change only
 #                                severity for known errors, and 
 #                                improved code readability
+# 2013/18/11 v1.0.10 Brenn Oosterbaan - handle NMS unresponiveness
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
 # Schuberg Philis 2012
@@ -50,6 +51,7 @@ import os
 import sys
 import urllib2
 import socket
+import time
 
 try:
     import json
@@ -147,16 +149,23 @@ class NexentaApi:
         request.add_header('Authorization', 'Basic %s' % self.base64_string)
         request.add_header('Content-Type' , 'application/json')
 
-        # Try to connect max 2 times.
+        # Try to connect max 2 times. Sleep 20 seconds if NMS connection error occurs.
         tries = 2
         while tries:
             try:
                 response = json.loads(urllib2.urlopen(request).read())
+                if response['error'] and "Cannot introspect object com.nexenta.nms" in response['error']['message']:
+                    raise Exception("NMS unresponsive")
                 break
-            except urllib2.URLError:
+            except (urllib2.URLError, Exception):
                 tries += -1
+                time.sleep(20)
+
         if not tries:
-            raise CritError("Unable to connect to API at %s" % (self.url))
+            if response['error'] and "Cannot introspect object com.nexenta.nms" in response['error']['message']:
+                raise CritError("API error occured: NMS unresponsive")
+            else:
+                raise CritError("Unable to connect to API at %s" % (self.url))
 
         if response['error']:
             raise CritError("API error occured: %s" % response['error'])
@@ -644,7 +653,7 @@ def print_usage():
     sys.exit()
 
 def print_version():
-    print "Version 1.0.9"
+    print "Version 1.0.10"
     sys.exit()
 
 if __name__ == '__main__':
